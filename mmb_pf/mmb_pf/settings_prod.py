2b7199bc90c2ad3c8f2ac958a3e03acebf6c9b9d
Artem <fzeulf@gmail.com>
2021-11-22 13:00:15 +0300

    add addrbook app

--- mmb_pf/mmb_pf/settings_prod.py
+++ mmb_pf/mmb_pf/settings_prod.py
@@ -29,7 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_&%83(h7ix6k8et0mjem$mv-@+3!4&@v^6^yr3o_ldl5zb50)w"
+# For regeneration
+# from django.core.management.utils import get_random_secret_key
+# get_random_secret_key()
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
