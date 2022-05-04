4e111da0b764e74d3877690a69a1d808b7045bc0
art.antonov <art.antonov@corp.mail.ru>
2022-05-04 13:49:55 +0300

    Release v1.0.1

--- mmb_pf/mmb_pf/settings_example.py
+++ mmb_pf/mmb_pf/settings_example.py
@@ -15,7 +15,7 @@ import os
 from datetime import datetime
 from pathlib import Path
 
-MMB_PF_VERSION = "1.0.0"
+MMB_PF_VERSION = "1.0.1"
 logger = logging.getLogger(__name__)
 logger.critical(
     f"{'#'*80}\n# MMB PF v{MMB_PF_VERSION} started: {datetime.now().strftime('%d.%m.%Y %H:%M')}\n{'#'*80}\n"
